09f021acf5022d99d2c54693a3204214cf3142cc
Soumya-ui-bytes <soumyarajagopal027@gmail.com>
2025-11-11 11:50:05 +0530

    saved

--- bonus.py
+++ bonus.py
@@ -2,7 +2,7 @@ import sys
 if len(sys.argv) == 3:
   script_name = sys.argv[0]
   salary = float(sys.argv[1])
-  bonus = float(sys.argv[2])*0.10
+  bonus = float(sys.argv[2])/0.10
   print("user provided input values:")
   print("script name:", script_name)
   print("salary:", salary)
@@ -12,7 +12,7 @@ if len(sys.argv) == 3:
 else:
   script_name = sys.argv[0]
   salary = "1000"
-  bonus = "0.10"
+  bonus = "10000 * 0.10"
   print("no input given:")
   print("script name:", script_name)
   print("salary:", salary)
